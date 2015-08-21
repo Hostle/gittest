20eef0dadb7efa634b392190b30693124387aef8
Hostle <hostle19@gmail.com>
2015-08-20 20:50:44 -0400

    Update gettest.c

--- src/gettest.c
+++ src/gettest.c
@@ -1,5 +1,7 @@
-#inlcude <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
+#define VERSION = GIT_COMMIT
+
 printf("This is a get commit test\n");
-printf("version : %s\n",version);
+printf("version : %s\n",VERSION);
